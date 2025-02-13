877d3173c558e3c78eada87e2d758400b67095f5
yyuntzpan <yap0602@gmail.com>
2025-02-13 12:51:26 +0800

    設定pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
+const pinia = createPinia()
+const app = createApp(App)
 
-createApp(App).mount('#app')
+app.use(pinia)
+app.mount('#app')
